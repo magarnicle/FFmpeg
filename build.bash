f4ab9889b1eb049c5793d3b0870a43ae7088a2b0
Matthew Rademaker <matthew@acctv.com.au>
2025-09-11 13:19:05 +1000

    build against blackmagic SDK 15

--- build.bash
+++ build.bash
@@ -2,7 +2,7 @@
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
 --prefix="$HOME/ffmpeg_build" \
 --pkg-config-flags="--static" \
---extra-cflags="-I$HOME/ffmpeg_build/include -I$HOME/ffmpeg_sources/BMD_SDK_latest/include" \
+--extra-cflags="-I$HOME/ffmpeg_build/include -I$HOME/ffmpeg_sources/Blackmagic_DeckLink_SDK_15.0/Linux/include" \
 --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
 --extra-libs="-lpthread -lm" \
 --bindir="$HOME/bin" \
