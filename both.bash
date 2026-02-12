31edf37752c164362ae10b189400f1405ff6eb64
Matthew Rademaker <matthew@acctv.com.au>
2026-02-13 09:27:28 +1000

    minor fixes

--- both.bash
+++ both.bash
@@ -1,2 +1,8 @@
-./ffmpeg -i test2.mov -i test2.srt -filter_complex "[0:v][1:s]cea608embed[vid]" -map "[vid]" -map "0:a" -map "1:s" -c:v v210 -c:s teletext -f decklink "DeckLink SDI 4K"
-#-a53cc 1 
+set -o errexit
+set -o xtrace
+set -o pipefail
+while true
+do
+    ./ffmpeg -v debug -i test2.mov -i test2.srt -filter_complex "[0:v][1:s]cea608embed[vid]" -map "[vid]" -map "0:a" -map "1:s" -c:v v210 -c:s teletext -a53cc 1 -f decklink "DeckLink SDI 4K" 2>&1 | tee both.txt
+   sleep 60
+done
