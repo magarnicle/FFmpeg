af9e580257bdd900e6daa687c0d8f265661804ca
Matthew Rademaker <matthew@acctv.com.au>
2026-02-12 16:26:09 +1000

    fix for both.bash

--- both.bash
+++ both.bash
@@ -1,1 +1,2 @@
-./ffmpeg -i test2.mov -i test2.srt -filter_complex [0:v][1:s]cea608embed[vid] -map [vid] -map 0:a -map 1:s -c:v v210 -a53cc 1 -c:s teletext -f decklink DeckLink SDI 4K
+./ffmpeg -i test2.mov -i test2.srt -filter_complex "[0:v][1:s]cea608embed[vid]" -map "[vid]" -map "0:a" -map "1:s" -c:v v210 -c:s teletext -f decklink "DeckLink SDI 4K"
+#-a53cc 1 
